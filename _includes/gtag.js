282b507d262b568e099a7018ed71e1b55b641fca
Matic Basle <maticbasle@users.noreply.github.com>
2020-11-29 10:33:18 +0100

    Require permission for GA

---
+++ _includes/gtag.js
@@ -0,0 +1,7 @@
+async src="https://www.googletagmanager.com/gtag/js?id=G-PQXJQGY6MK"
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-PQXJQGY6MK');
