be43b8678c8a647426bc42dd66d62f925c9da64f
Matic Basle <maticbasle@users.noreply.github.com>
2020-11-29 11:03:25 +0100

    GA Fix

--- _includes/gtag.js
+++
@@ -1,7 +0,0 @@
-async src="https://www.googletagmanager.com/gtag/js?id=G-PQXJQGY6MK"
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-PQXJQGY6MK');
